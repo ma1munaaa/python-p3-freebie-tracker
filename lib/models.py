f0336402e3a7edb10231a4885070665361a1547c
maimuna <mohamudmaimuna003@gmail.com>
2023-09-09 09:21:38 +0300

    done

--- lib/models.py
+++ lib/models.py
@@ -1,29 +1,67 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Table, Column, Integer, String, MetaData, create_engine
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
+# Define the naming convention for foreign keys
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
+
+# Create the metadata object with the naming convention
 metadata = MetaData(naming_convention=convention)
 
+# Create the base class for declarative models
 Base = declarative_base(metadata=metadata)
 
+# Define the many-to-many relationship table
+company_dev = Table(
+    'company_dev',
+    Base.metadata,
+    Column('company_id', ForeignKey('companies.id'), primary_key=True),
+    Column('dev_id', ForeignKey('devs.id'), primary_key=True),
+    extend_existing=True,
+)
+
+# Define the Company model
 class Company(Base):
     __tablename__ = 'companies'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
+    Freebie = relationship("Freebie", backref=backref("company"))
+    devs = relationship("Dev", secondary=company_dev, back_populates="companies")
 
     def __repr__(self):
-        return f'<Company {self.name}>'
+        return f'Company {self.name}, Founding year {self.founding_year}'
 
+# Define the Dev model
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
+    Freebie = relationship("Freebie", backref=backref("dev"))
+    companies = relationship("Company", secondary=company_dev, back_populates="devs")
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+# Define the Freebie model
+class Freebie(Base):
+    __tablename__ = "Freebie"
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    Value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey("companies.id"))
+    dev_id = Column(Integer(), ForeignKey("devs.id"))
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}>'
+
+# Create the engine and metadata objects
+engine = create_engine('sqlite:///freebies.db')
+
+# Create the tables in the database
+Base.metadata.create_all(engine)
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,55 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+from faker import Faker
+import random
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Company, Dev, Freebie
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///freebies.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Dev).delete()
+    session.query(Company).delete()
+    session.query(Freebie).delete()
+
+    fake = Faker()
+    
+    # Populate Companies
+    for _ in range(10):  # Create 10 fake companies
+        company = Company(
+            name=fake.company(),
+            founding_year=fake.year(),
+        )
+        session.add(company)
+    
+    # Populate Devs
+    for _ in range(15):  # Create 15 fake developers
+        dev = Dev(
+            name=fake.name(),
+        )
+        session.add(dev)
+
+    # Populate Freebies
+    for _ in range(30):  # Create 30 fake freebies
+        freebie = Freebie(
+            item_name=fake.word(),
+            Value=fake.random_int(min=1, max=100),
+            company_id=random.choice(session.query(Company).all()).id,  # Randomly assign a company
+            dev_id=random.choice(session.query(Dev).all()).id,  # Randomly assign a developer
+        )
+        session.add(freebie)
+    
+    for company in session.query(Company).all():
+        for dev in fake.random_elements(
+            elements=session.query(Dev).all(),
+            length=random.randint(1, 5),  # Random number of devs associated with each company
+            unique=True,
+        ):
+            company.devs.append(dev)
+
+    session.commit()
+    session.close()
